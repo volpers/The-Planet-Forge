05498dd1fea02bce2a19948bc808ba0628597f76
volpers <nlskleine@gmail.com>
2017-08-11 21:38:32 +0200

    Created projects

---
+++ The-Planet-Forge-Implementation/The-Planet-Forge-Implementation/src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Planet_Forge_Implementation.src
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main() {
+
+            MainFrame frame = new MainFrame("The Planet Forge");
+            Application.EnableVisualStyles();           
+            Application.Run(frame);
+
+        }                                
+    }
+}
---
+++ The-Planet-Forge-Implementation/The-Planet-ForgeTests/src/renderer/CoreRendererTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using The_Planet_Forge_Implementation.src.renderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Planet_Forge_Implementation.src.renderer.Tests
+{
+    [TestFixture()]
+    public class CoreRendererTests
+    {
+        [Test()]
+        public void InitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
